628723329b26494fe629b26808d4e9bb536c8907
Reve1io <povezlo.tak@mail.ru>
2025-11-17 12:38:03 +0300

    update connection to local db

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -10,13 +10,11 @@ import 'dotenv/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        url: configService.get('DATABASE_URL'),
-        /*
         host: configService.get('DB_HOST', 'localhost'),
         port: configService.get('DB_PORT', 5432),
-        username: configService.get('DB_USERNAME', 'postgres'),
-        password: configService.get('DB_PASSWORD', 'Romanov-9653.'),
-        database: configService.get('DB_NAME', 'Quantra'), */
+        username: configService.get('DB_USERNAME', 'flawyadmin'),
+        password: configService.get('DB_PASSWORD', 'romanov9653'),
+        database: configService.get('DB_NAME', 'quantra_db'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: configService.get('DB_SYNCHRONIZE', false), // false для production
         logging: configService.get('DB_LOGGING', true),
